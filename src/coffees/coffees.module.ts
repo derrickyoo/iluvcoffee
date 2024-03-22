39c88def3352b887119b2d46c65a967cd3ed94dc
Derrick Yoo <derrickyoo@gmail.com>
2024-03-22 08:33:18 -0600

    Coffees module

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
+import { CoffeesController } from './coffees.controller';
+import { CoffeesService } from './coffees.service';
 
 // Provides metadata that NestJS uses to organize the application structure
 @Module({
   // API routes instantiated by this module
-  controllers: [],
+  controllers: [CoffeesController],
   // Providers that should be available anywhere this module is imported
   exports: [],
   // List other modules that this module requires
   imports: [],
   // Services that need to be instantiated by the NestJS injector (can also be added to exports if you want to expose them to other modules that consume this module)
-  providers: [],
+  providers: [CoffeesService],
 })
 export class CoffeesModule {}
