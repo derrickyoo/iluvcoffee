d140bd2c51e24f4efe9d00a290a718776949134e
Derrick Yoo <derrickyoo@gmail.com>
2024-03-23 11:15:53 -0600

    Many-to-many relationship

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
 
 // Provides metadata that NestJS uses to organize the application structure
 @Module({
@@ -11,7 +12,7 @@ import { Coffee } from './entities/coffee.entity';
   // Providers that should be available anywhere this module is imported
   exports: [],
   // List other modules that this module requires
-  imports: [TypeOrmModule.forFeature([Coffee])],
+  imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
   // Services that need to be instantiated by the NestJS injector (can also be added to exports if you want to expose them to other modules that consume this module)
   providers: [CoffeesService],
 })
