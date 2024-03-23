df55e5d77b29a245697537ecf3306f8f72edd652
Derrick Yoo <derrickyoo@gmail.com>
2024-03-23 08:46:02 -0600

    Input validation with dto

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,5 +1,5 @@
-export class UpdateCoffeeDto {
-  readonly name?: string;
-  readonly brand?: string;
-  readonly flavors?: string[];
-}
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCoffeeDto } from './create-coffee.dto';
+
+// PartialType marks all fields as optional and also inherits all validation rules applied via decorators as well as adds a single valudation rule to each field that is optional
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
