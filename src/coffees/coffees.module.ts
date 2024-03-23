158ee107a7a381894dcc1c5a24997fcd86201d03
Derrick Yoo <derrickyoo@gmail.com>
2024-03-23 10:30:57 -0600

    Create TypeORM entity

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { Coffee } from './entities/coffee.entity';
 
 // Provides metadata that NestJS uses to organize the application structure
 @Module({
@@ -9,7 +11,7 @@ import { CoffeesService } from './coffees.service';
   // Providers that should be available anywhere this module is imported
   exports: [],
   // List other modules that this module requires
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Coffee])],
   // Services that need to be instantiated by the NestJS injector (can also be added to exports if you want to expose them to other modules that consume this module)
   providers: [CoffeesService],
 })
