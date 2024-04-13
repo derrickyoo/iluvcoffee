51f901247d805f5e705ca07654e62f37f14a8abc
Derrick Yoo <derrick.yoo@equips.com>
2024-04-13 09:43:48 -0700

    Config .env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,16 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule,
+    // `forRoot` is a static method that will load and parse the process.env file from the default location (project root directory)
+    ConfigModule.forRoot(),
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT, // everything from process .env is a string so we cast it to a number using "+"
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true, // load modules automatically
       synchronize: true, // ensures TypeORM entites are synced with the DB everytime we run our application (disable this in production)
     }),
