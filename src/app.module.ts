531d88c8321bee7130a70aa3050b2c84d9b8bcb0
Derrick Yoo <derrick.yoo@equips.com>
2024-04-14 08:13:33 -0700

    Best practice for app to be deterministic and not worry about module imports order

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,15 +18,17 @@ import appConfig from './config/app.config';
       }),
     }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT, // everything from process .env is a string so we cast it to a number using "+"
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true, // load modules automatically
-      synchronize: true, // ensures TypeORM entites are synced with the DB everytime we run our application (disable this in production)
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT, // everything from process .env is a string so we cast it to a number using "+"
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true, // load modules automatically
+        synchronize: true, // ensures TypeORM entites are synced with the DB everytime we run our application (disable this in production)
+      }),
     }),
   ],
   controllers: [AppController],
