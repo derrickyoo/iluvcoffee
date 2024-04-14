dcaa9970e7f0b54903f51e0e4b194d7efacecf69
Derrick Yoo <derrick.yoo@equips.com>
2024-04-14 10:06:16 -0700

    Catch exceptions with filters

---
+++ src/common/filters/http-exception/http-exception.filter.ts
@@ -0,0 +1,30 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter<T extends HttpException>
+  implements ExceptionFilter
+{
+  catch(exception: T, host: ArgumentsHost) {
+    const ctx = host.switchToHttp(); // Gives us access to the native inflight request or response objects
+
+    const response = ctx.getResponse<Response>(); // Gives us the underlying platform response object (e.g. Express by default)
+
+    const status = exception.getStatus();
+    const exceptionResponse = exception.getResponse();
+    const error =
+      typeof response === 'string'
+        ? { message: exceptionResponse }
+        : (exceptionResponse as object);
+
+    response.status(status).json({
+      ...error,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
