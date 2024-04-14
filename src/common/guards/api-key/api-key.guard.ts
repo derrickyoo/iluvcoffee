e0ce778ef57d63ac8536ef18f4c52f6ae7618031
Derrick Yoo <derrick.yoo@equips.com>
2024-04-14 10:25:47 -0700

    Add guard

--- src/common/guards/api-key/api-key.guard.ts
+++ src/common/guards/api-key/api-key.guard.ts
@@ -1,4 +1,5 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Request } from 'express';
 import { Observable } from 'rxjs';
 
 @Injectable()
@@ -6,6 +7,8 @@ export class ApiKeyGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    const request = context.switchToHttp().getRequest<Request>();
+    const authHeader = request.header('Authorization');
+    return authHeader === process.env.API_KEY; // TODO: utilize the ConfigService
   }
 }
